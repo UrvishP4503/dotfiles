0ca66d76fbe7290f6ffc8749e8c64a401b9f4099
Urvish <urvishp45003@gmail.com>
2023-05-17 18:25:17 +0530

    Almost done

---
+++ nvim/init.lua
@@ -0,0 +1,13 @@
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.ch"
+require "user.cmp"
+require "user.lsp"
+--TODO:todo-comments
+--TODO:LSP for python,java,go and shell
+--TODO:Treesitter
+--TODO:Telescope
+--TODO:Mason
+--TODO:lazygit
+--TODO:Gruvbox
