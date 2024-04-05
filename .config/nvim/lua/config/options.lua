28ad975248b49b556b31f904f69dc80a3b290cf5
Urvish <urvishp45003@gmail.com>
2024-04-05 20:40:26 +0530

    tmux and vim things

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
+
+local opt = vim.opt
+
+opt.scrolloff = 8 -- Lines of context
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.expandtab = true
+opt.softtabstop = 4
